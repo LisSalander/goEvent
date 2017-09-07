8d18ea46e32f85b06bab8b757fd9c662a001020c
vika <vika@192.168.33.130.local.ipnet.ua>
2017-09-08 00:15:44 +0300

    goEvent

--- goEventS/MenuView.swift
+++ goEventS/MenuView.swift
@@ -8,16 +8,10 @@
 
 import UIKit
 
-class MenuView: UIViewController{
+class MenuView: UITableViewController{
     
-    //var nameCellArray = [String]()
-
-   // @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-       // nameCellArray = ["main","event list","map"]
-        
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,30 +19,5 @@ class MenuView: UIViewController{
         // Dispose of any resources that can be recreated.
     }
     
-    /*func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = nameCellArray[indexPath.row]
-        
-        return cell
-    }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        
-        switch indexPath.row {
-        case 0:
-            // ContainerVC.swift listens for this
-            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "openModalWindow"), object: nil)
-        case 1:
-            // Both FirstViewController and SecondViewController listen for this
-            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "openPushWindow"), object: nil)
-        default:
-            print("indexPath.row:: \(indexPath.row)")
-        }
-    }*/
-    
-}
+    }
